a0a16485be01b9d073aac6a62d2215da5881d831
Max <maxhroberts@gmail.com>
2018-10-21 19:55:20 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,10 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot (argument)
-  if argument.to_s.length > 0
+  unless argument.to_s.length == 0
     puts argument
     return argument
   else
+    puts "Squawk!"
     return "Squawk!"
   end
 end
